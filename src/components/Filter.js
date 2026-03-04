c567a9fb2054fb28afd1f369d9d8a7186831719b
malyunharedabdi <mankaharet48@gmail.com>
2026-03-04 10:50:43 +0300

    done

---
+++ src/components/Filter.js
@@ -0,0 +1,14 @@
+function Filter({ onCategoryChange }) {
+    return (
+        <div className="Filter">
+            <select onChange={(e) => onCategoryChange(e.target.value)}>
+                <option value="All">All</option>
+                <option value="Produce">Produce</option>
+                <option value="Dairy">Dairy</option>
+                <option value="Dessert">Dessert</option>
+            </select>
+        </div>
+    );
+}
+
+export default Filter;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header({ onDarkModeClick }) {
+    return (
+        <header>
+            <h1>Shopping List</h1>
+            <button onClick={onDarkModeClick}>
+                Toggle Dark Mode
+            </button>
+        </header>
+    );
+}
+
+export default Header;
